2a715fca44eaa9b82654b2c1ad08c77183091f26
Purple4pur <purple4pur@gmail.com>
2021-06-20 00:05:00 +0800

    显示指针（静态），改用 JLayeredPane

--- src/main/App.java
+++ src/main/App.java
@@ -15,6 +15,7 @@ public class App extends JFrame {
     final int height = 300;
     int hour, minute, second;
 
+    JLayeredPane mainPanel = new JLayeredPane();
     Dial dial;
     Pointer pointer;
 
@@ -27,10 +28,14 @@ public class App extends JFrame {
         second = time.getSecond();
 
         dial = new Dial(120);
-        pointer = new Pointer(80, 100, 110);
+        pointer = new Pointer(70, 90, 100);
         pointer.setTime(hour, minute, second);
 
-        add(dial);
+        dial.setBounds(0, 0, width, height); // 在 JLayeredPane 中显示必须 setBounds
+        pointer.setBounds(0, 0, width, height);
+        mainPanel.add(dial, 1);
+        mainPanel.add(pointer, 2);
+        setContentPane(mainPanel);
 
         setSize(width, height);
         setResizable(false);
@@ -44,6 +49,8 @@ public class App extends JFrame {
 class Dial extends JPanel {
     int r;
     final double radDelta = Math.toRadians(30); // 30°
+    final Stroke boldStroke = new BasicStroke(2);
+    final Stroke regularStroke = new BasicStroke(1);
 
     public Dial(int radius) {
         r = radius;
@@ -53,8 +60,6 @@ class Dial extends JPanel {
         Graphics2D g2d = (Graphics2D) g;
         g2d.translate(142, 130); // ?
 
-        Stroke boldStroke = new BasicStroke(2);
-        Stroke regularStroke = new BasicStroke(1);
         g2d.setStroke(boldStroke);
         g2d.drawOval(-r, -r, r*2, r*2);
         
@@ -85,9 +90,12 @@ class Dial extends JPanel {
 // 指针
 class Pointer extends JPanel {
     int hourLen, minLen, secLen;
-    int hour = 0;
-    int minute = 0;
-    int second = 0;
+    double hour = 0; // 0 ~ 60
+    double minute = 0; // 0 ~ 60
+    double second = 0; // 0 ~ 60
+    final double radDelta = Math.toRadians(6); // 6°
+    final Stroke boldStroke = new BasicStroke(2);
+    final Stroke regularStroke = new BasicStroke(1);
 
     public Pointer(int hourLength, int minuteLength, int secondLength) {
         hourLen = hourLength;
@@ -96,8 +104,41 @@ class Pointer extends JPanel {
     }
 
     public void setTime(int hour, int minute, int second) {
-        this.hour = hour;
-        this.minute = minute;
         this.second = second;
+        this.minute = minute + this.second/60;
+        this.hour = (hour + this.minute/60) * 5;
+    }
+
+    public void paint(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.translate(142, 130);
+        g2d.setStroke(boldStroke);
+
+        // 时针
+        g2d.setColor(Color.RED);
+        g2d.drawLine(
+            0,
+            0,
+            (int) (hourLen * Math.sin(radDelta * hour)),
+            (int) (-hourLen * Math.cos(radDelta * hour))
+        );
+
+        // 分针
+        g2d.setColor(Color.BLUE);
+        g2d.drawLine(
+            0,
+            0,
+            (int) (minLen * Math.sin(radDelta * minute)),
+            (int) (-minLen * Math.cos(radDelta * minute))
+        );
+
+        // 秒针
+        g2d.setColor(Color.GREEN);
+        g2d.drawLine(
+            0,
+            0,
+            (int) (secLen * Math.sin(radDelta * second)),
+            (int) (-secLen * Math.cos(radDelta * second))
+        );
     }
 }
